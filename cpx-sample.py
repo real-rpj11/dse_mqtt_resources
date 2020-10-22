0d3d7a801ce4d11ffd9e1771641253aa93e17a58
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-10-22 17:21:40 +0800

    Removed unnecessary codes

--- cpx-sample.py
+++ cpx-sample.py
@@ -1,28 +1,20 @@
-"""
-To get started, check out the "Device Simulator Express: Getting Started" command in the command pallete, which you can access with `CMD + SHIFT + P` For Mac and `CTRL + SHIFT + P` for Windows and Linux.
-
-Getting started with CPX and CircuitPython intro on:
-https://learn.adafruit.com/circuitpython-made-easy-on-circuit-playground-express/circuit-playground-express-library
-
-Find example code for CPX on:
-https://github.com/adafruit/Adafruit_CircuitPython_CircuitPlayground/tree/master/examples
-"""
-
-# import CPX library
 from adafruit_circuitplayground import cp
-from time import sleep
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe("cloud")
+    # print("Connected with result code "+str(rc))
+    if rc == 0:
+        cp.red_led = True
+        client.subscribe("cpx-switch")
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+msg.payload.decode())
-    if msg.payload.decode() == "on":
+    # print(msg.topic+" "+msg.payload.decode())
+    if msg.payload.decode() == "true":
         cp.pixels[0] = (255, 255, 255)
     else:
         cp.pixels[0] = (0, 0, 0)
+
+cp.red_led = False
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -32,14 +24,6 @@ client.connect("mqtt.eclipse.org", 1883, 60)
 
 client.loop_forever()
 
-# while True:
-#     cp.pixels[0] = (255, 255, 255)
-#     sleep(1)
-#     cp.pixels[0] = (0, 0, 0)
-#     sleep(1)
-#     # start your code here
-#     # pass
-
-
-# Broker for online client in github
+# Broker for online client: https://iamelijah2016.github.io/
 # wss://mqtt.eclipse.org:443/mqtt
+# wss://test.mosquitto.org:8081/mqtt
